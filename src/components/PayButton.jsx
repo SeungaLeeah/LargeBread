56841fc8deb3e27cfaba5e712ec5723802e354e0
juaehan <71582626+juaehan@users.noreply.github.com>
2022-08-17 15:31:53 +0900

    2022-08-17 >> hja 장바구니 수정

--- src/components/PayButton.jsx
+++ src/components/PayButton.jsx
@@ -1,7 +1,7 @@
-import React, { memo, useCallback } from 'react';
+import React, { memo, useCallback,useEffect } from 'react';
 import styled from "styled-components";
-import { useNavigate } from 'react-router-dom';
-import { useSelector,useDispatch } from 'react-redux';
+import { useNavigate,useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 import { addCart } from '../slices/BasketSlice';
@@ -42,8 +42,9 @@ const PayBtn = styled.form`
 const PayButton = memo(({id, amount}) => {
     const PaySwal = withReactContent(Swal);
     const navigate = useNavigate();
-    const params = useParams();
- 
+    const basket = useSelector((state) => state.basket);
+    console.log(basket);
+ console.log(basket.basketItems[0].id);
     const dispatch = useDispatch();
     
     // Promise 방식을 사용한 다이얼로그
@@ -68,9 +69,9 @@ const PayButton = memo(({id, amount}) => {
             /* Read more about handling dismissals below */
             if (result.dismiss === Swal.DismissReason.timer) {
                 
-              dispatch(postItem({
-                id: params.id,
-                amount: params.amount
+              dispatch(addCart({
+                product_id: basket.basketItems[0].id,
+                amount: basket.basketTotalQuantity
               }))
               navigate('/');
               PaySwal.fire({
@@ -79,11 +80,11 @@ const PayButton = memo(({id, amount}) => {
               });
             }
           })
-    }, [PaySwal,navigate,dispatch,params.id,params.amount]);
+    }, [PaySwal,navigate,dispatch]);
     return (
         <PayBtn >
           <div onClick={onSubmit} className='item-payBtn'>
-            <input className='submitBtn' name='submit' value="결제하기" type="submit" readOnly/>
+            <input className='submitBtn' name='submit' value="결제하기" type="submit"/>
             </div>
         </PayBtn>
     );
